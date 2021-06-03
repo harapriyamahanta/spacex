59a76996fefa1e87668d006d4cf36fbf12d6c503
harapriya <harapriya.mahanta@techunits.com>
2021-06-03 21:42:13 +0530

    task for spacex

---
+++ application/views/welcome.php
@@ -0,0 +1,165 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>SpaceX Lunch</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url().'assets/css/custom.css'; ?>">
+	
+</head>
+<body style="">
+
+<div class="container1" >
+	<h1>SpaceX Lunch Programs</h1>
+	<div class="row">
+		<div class="col-lg-3 col-md-4 col-xs-12">
+			<div class="card">
+				<div class="card-body">
+			       <h3>Filters</h3>
+			       <div class="text-center pr-5 pl-5">
+			       		<p class="borderbtom">Lunch Year</p>
+			       </div>
+		        	
+			       <div class="text-center pr-5 pl-5">
+				      	<div class="row pb-1">
+						    <div class="col-6">
+						    	<a class="btn btn-primary" data-type="launch_year" data-id="2006" href="javascript:void(0)">2006</a>
+						    </div>
+						    <div class="col-6">
+						    	<a class="btn btn-primary" data-type="launch_year" data-id="2007" href="javascript:void(0)">2007</a>
+						    </div>
+				      	</div>
+
+				      	<div class="row  pb-1">
+						    <div class="col-6">
+						    	<a class="btn btn-primary" data-type="launch_year" data-id="2008" href="javascript:void(0)">2008</a>
+						    </div>
+						    <div class="col-6">
+						    	<a class="btn btn-primary" data-type="launch_year" data-id="2009" href="javascript:void(0)">2009</a>
+						    </div>
+				      	</div>
+
+				      	<div class="row pb-1">
+						    <div class="col-6">
+						    	<a class="btn btn-primary" data-type="launch_year" data-id="2010" href="javascript:void(0)">2010</a>
+						    </div>
+						    <div class="col-6">
+						    	<a class="btn btn-primary" data-type="launch_year" data-id="2011" href="javascript:void(0)">2011</a>
+						    </div>
+				      	</div>
+
+				      	<div class="row pb-1">
+						    <div class="col-6">
+						    	<a class="btn btn-primary" data-type="launch_year" data-id="2012" href="javascript:void(0)">2012</a>
+						    </div>
+						    <div class="col-6">
+						    	<a class="btn btn-primary" data-type="launch_year" data-id="2013" href="javascript:void(0)">2013</a>
+						    </div>
+				      	</div>
+
+				      	<div class="row pb-1">
+						    <div class="col-6">
+						    	<a class="btn btn-primary" data-type="launch_year" data-id="2014" href="javascript:void(0)">2014</a>
+						    </div>
+						    <div class="col-6">
+						    	<a class="btn btn-primary" data-type="launch_year" data-id="2015" href="javascript:void(0)">2015</a>
+						    </div>
+				      	</div>
+
+				      	<div class="row pb-1">
+						    <div class="col-6">
+						    	<a class="btn btn-primary" data-type="launch_year" data-id="2016" href="javascript:void(0)">2016</a>
+						    </div>
+						    <div class="col-6">
+						    	<a class="btn btn-primary" data-type="launch_year" data-id="2017" href="javascript:void(0)">2017</a>
+						    </div>
+				      	</div>
+
+				   		<div class="row pb-1">
+						    <div class="col-6">
+						    	<a class="btn btn-primary" data-type="launch_year" data-id="2018" href="javascript:void(0)">2018</a>
+						    </div>
+						    <div class="col-6">
+						    	<a class="btn btn-primary" data-type="launch_year" data-id="2019" href="javascript:void(0)">2019</a>
+						    </div>
+				      	</div>
+
+				      	<div class="row pb-1">
+						    <div class="col-6">
+						    	<a class="btn btn-primary" data-type="launch_year" data-id="2020" href="javascript:void(0)">2020</a>
+						    </div>
+						    <div class="col-6">
+						    	<a class="btn btn-primary" data-type="launch_year" data-id="2021" href="javascript:void(0)">2021</a>
+						    </div>
+				      	</div>
+
+
+			      	</div>
+			      	
+			      	<div class="text-center pr-5 pl-5">
+			       		<p class="borderbtom">Successful Lunch</p>
+			        </div>
+			        <div class="text-center pr-5 pl-5">
+				      	
+
+				      	<div class="row  pb-1">
+						    <div class="col-6">
+						    	<a class="btn btn-primary" data-type="launch_success" data-id="true" href="javascript:void(0)">True</a>
+						    </div>
+						    <div class="col-6">
+						    	<a class="btn btn-primary" data-type="launch_success" data-id="false" href="javascript:void(0)">False</a>
+						    </div>
+				      	</div>
+				    </div>
+			      	
+
+			      	
+			      	<div class="text-center pr-5 pl-5">
+			       		<p class="borderbtom">Successful Landing</p>
+			        </div>
+			        <div class="text-center pr-5 pl-5">
+				      	
+
+				      	<div class="row  pb-1">
+						    <div class="col-6">
+						    	<a class="btn btn-primary" data-type="land_success" data-id="true" href="javascript:void(0)">True</a>
+						    </div>
+						    <div class="col-6">
+						    	<a class="btn btn-primary" data-type="land_success" data-id="false" href="javascript:void(0)">False</a>
+						    </div>
+				      	</div>
+				    </div>
+			      	
+		      </div>
+	      </div>
+	    </div>
+	    <div class="col-lg-9 col-md-8 col-xs-12">
+	    	<div class="row pr-3 pl-3" id="searchlist">
+	      		<div>Loading ............</div>
+	      	</div>
+	    </div>
+	</div>
+	<footer class="footer text-center">
+                Devloped By:Harapriya Mahanta
+    </footer>
+</div>
+
+</body>
+
+<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" ></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" ></script>
+
+<script  src="<?php echo base_url().'assets/js/custom.js'; ?>"></script>
+<script type="text/javascript">
+	getResult();
+
+</script>
+</html>
---
+++ assets/js/custom.js
@@ -0,0 +1,89 @@
+function getResult(param){
+
+	$.ajax({
+        type:'get',
+        url:'https://api.spaceXdata.com/v3/launches?limit=100'+param,
+        
+        success:function(data)
+        {   
+        	var searchlist='';
+        	console.log(data.length);
+        	if(data.length>0){
+	        	$.each(data,function(index,item){
+	        			
+	        			searchlist+='<div class="col-lg-3 col-md-6 col-xs-12 box">\
+		      			<div class="card">\
+							<div class="card-body">\
+					        	<div class="img-box">\
+						      		<img class="card-img-top" src="'+item.links.mission_patch_small+'" alt="Card image cap">\
+						      	</div>\
+						      	<div class="row"><div class="col-12"><h5 class="card-title">'+item.mission_name+' #'+item.flight_number+'</h5></div></div>\
+							    <div class="row"><div class="col-6">Mission Ids: </div><div class="col-6">'+item.mission_id+'</div></div>\
+							    <div class="row"><div class="col-6">Lunch Year: </div><div class="col-6">'+item.launch_year+'</div></div>\
+							    <div class="row"><div class="col-6">Successful Lunch:</div><div class="col-6">'+item.launch_success+'</div></div>\
+							    <div class="row"><div class="col-6">Successful Landing:</div><div class="col-6"> '+item.rocket.first_stage.cores[0].land_success+'</div></div>\
+						    </div>\
+						</div>\
+				    </div>';
+	        	})
+        	}else{
+        		var searchlist='<div class="card col-md-12 col-sm-12 text-center pr-3 pl-3"><h3>No Record Found!</h3></div>';
+        	}
+        	$('#searchlist').html(searchlist);
+        }
+    });
+}
+
+
+$('a').on('click',function(){
+		var type = $(this).data('type');
+		var val = $(this).data('id');
+		if(type=='launch_year'){
+			$("a[data-type='launch_year']").removeClass('active');
+			$(this).addClass('active');
+			var param = '&launch_year='+val;
+			$("a[data-type='launch_success']").each(function(obj){
+				if($(this).hasClass("active")){
+				param += '&launch_success='+$(this).data('id');
+				}
+			});
+			$("a[data-type='land_success']").each(function(obj){
+				if($(this).hasClass("active")){
+				param += '&land_success='+$(this).data('id');
+				}
+			});
+		}
+		if(type=='launch_success'){
+			$("a[data-type='launch_success']").removeClass('active');
+			$(this).addClass('active');
+			var param = '&launch_success='+val;
+			$("a[data-type='launch_year']").each(function(obj){
+				if($(this).hasClass("active")){
+				param += '&launch_year='+$(this).data('id');
+				}
+			});
+			$("a[data-type='land_success']").each(function(obj){
+				if($(this).hasClass("active")){
+				param += '&land_success='+$(this).data('id');
+				}
+			});
+		}
+		if(type=='land_success'){
+			$("a[data-type='land_success']").removeClass('active');
+			$(this).addClass('active');
+			var param = '&land_success='+val;
+			$("a[data-type='launch_success']").each(function(obj){
+				if($(this).hasClass("active")){
+				param += '&launch_success='+$(this).data('id');
+				}
+			});
+			$("a[data-type='launch_year']").each(function(obj){
+				if($(this).hasClass("active")){
+				param += '&launch_year='+$(this).data('id');
+				}
+			});
+		}
+		getResult(param);
+	})
+
+
